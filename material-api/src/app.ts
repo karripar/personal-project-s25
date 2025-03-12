1b7e677996845d90934037161a24b4d664c1401d
karripartanen <karripartanen25@gmail.com>
2025-03-12 20:35:20 +0200

    update

--- material-api/src/app.ts
+++ material-api/src/app.ts
@@ -24,6 +24,7 @@ const corsOptions = {
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 };
 app.use(cors(corsOptions));
 
